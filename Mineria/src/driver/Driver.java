f7663f27546f54c70610eb63b380854d5712afd9
Marlon Nuno <marlini306@hotmail.com>
2017-03-02 16:31:08 -0600

    Nueva creación de Interfaz

--- Mineria/src/driver/Driver.java
+++ Mineria/src/driver/Driver.java
@@ -1,6 +1,6 @@
 package driver;
 
-import view.InterfazPrincipal;
+import view.Menu;
 
 public class Driver {
 
@@ -8,7 +8,7 @@ public class Driver {
 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new InterfazPrincipal().setVisible(true);
+                new Menu().setVisible(true);
             }
         });
     }
